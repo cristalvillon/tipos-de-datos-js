ec5a8e81d45ccf77343b2394b2d3eca978f1d359
cristal <cristalvillon00@gmail.com>
2025-01-27 15:03:33 -0300

    agregar ejemplos de tipos de datos en JavaScript

---
+++ tipos-de-datos.js
@@ -0,0 +1,10 @@
+// Valores básicos y sus tipos de datos
+console.log(typeof 42);           // number
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666);         // number
+console.log(typeof true);         // boolean
+console.log(typeof 0);            // number
+console.log(typeof '');           // string
+console.log(typeof null);         // object (peculiaridad en JS)
+console.log(typeof undefined);    // undefined
+console.log(typeof false);        // boolean
